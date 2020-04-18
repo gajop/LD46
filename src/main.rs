469612a502e87bb9be14da4b736ef17d150875f4
Gajo Petrovic <gajopetrovic@gmail.com>
2020-04-18 14:02:16 +0900

    - make spaceship controllable with wasd - use relative coords - wrapped world

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use ggez::event::{self, EventHandler};
+use ggez::conf;
+use ggez::event::{self, EventHandler, KeyCode, KeyMods};
 use ggez::nalgebra as na;
 use ggez::{graphics, Context, ContextBuilder, GameResult};
+
+const SCREEN_SIZE_X: f32 = 1000.0;
+const SCREEN_SIZE_Y: f32 = 720.0;
 
 fn main() {
     // Make a Context.
     let (mut ctx, mut event_loop) = ContextBuilder::new("save_the_pink_skins", "gajop")
+        .window_mode(conf::WindowMode::default().dimensions(SCREEN_SIZE_X, SCREEN_SIZE_Y))
         .build()
         .expect("Failed to create create ggez context. Please report this error");
 
@@ -15,25 +20,29 @@ fn main() {
     add_spaceship(
         &mut my_game,
         GameObject {
-            pos_x: 80.0,
-            pos_y: 340.0,
-            size_x: 10.0,
-            size_y: 10.0,
-            velocity_x: 0.0,
-            velocity_y: 0.0,
+            pos_x: 0.1,
+            pos_y: 0.3,
+            size_x: 0.01,
+            size_y: 0.01,
             color: graphics::Color::new(0.5, 0.5, 0.7, 1.0),
+            vel_x: 0.0,
+            vel_y: 0.0,
+            acc_x: 0.0,
+            acc_y: 0.0,
         },
     );
     add_earth(
         &mut my_game,
         GameObject {
-            pos_x: 500.0,
-            pos_y: 500.0,
-            size_x: 50.0,
-            size_y: 50.0,
-            velocity_x: 0.0,
-            velocity_y: 0.0,
+            pos_x: 0.5,
+            pos_y: 0.5,
+            size_x: 0.1,
+            size_y: 0.1,
             color: graphics::Color::new(0.3, 0.7, 0.3, 1.0),
+            vel_x: 0.0,
+            vel_y: 0.0,
+            acc_x: 0.0,
+            acc_y: 0.0,
         },
     );
 
@@ -49,35 +58,121 @@ struct GameObject {
     pos_y: f32,
     size_x: f32,
     size_y: f32,
-    velocity_x: f32,
-    velocity_y: f32,
+    vel_x: f32,
+    vel_y: f32,
+    acc_x: f32,
+    acc_y: f32,
     color: graphics::Color,
 }
+
+#[derive(Default)]
+struct Controls {
+    left_right: Option<Direction>,
+    up_down: Option<Direction>,
+}
+const MAX_ACC_X: f32 = 0.00005;
+const MAX_ACC_Y: f32 = 0.00005;
+const ACC_STEP_X: f32 = 0.00001;
+const ACC_STEP_Y: f32 = 0.00001;
 
 struct SaveThePinkSkin {
     // spaceship: GameObject,
     // earth: GameObject,
     objects: Vec<GameObject>,
+    spaceship_id: Option<usize>,
+    earth_id: Option<usize>,
+    controls: Controls,
 }
 
 fn add_spaceship(game: &mut SaveThePinkSkin, game_object: GameObject) {
-    game.objects.push(game_object)
+    game.objects.push(game_object);
+    game.spaceship_id = Some(game.objects.len() - 1);
 }
 
 fn add_earth(game: &mut SaveThePinkSkin, game_object: GameObject) {
-    game.objects.push(game_object)
+    game.objects.push(game_object);
+    game.earth_id = Some(game.objects.len() - 1);
 }
 
 impl SaveThePinkSkin {
     pub fn new(_ctx: &mut Context) -> SaveThePinkSkin {
         // Load/create resources such as images here.
-        SaveThePinkSkin { objects: vec![] }
+        SaveThePinkSkin {
+            objects: vec![],
+            controls: Default::default(),
+            spaceship_id: None,
+            earth_id: None,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+fn from_keycode(key: KeyCode) -> Option<Direction> {
+    match key {
+        KeyCode::Up | KeyCode::W => Some(Direction::Up),
+        KeyCode::Down | KeyCode::S => Some(Direction::Down),
+        KeyCode::Left | KeyCode::A => Some(Direction::Left),
+        KeyCode::Right | KeyCode::D => Some(Direction::Right),
+        _ => None,
     }
 }
 
 impl EventHandler for SaveThePinkSkin {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        // Update code here...
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        const TARGET_FPS: u32 = 60;
+
+        while ggez::timer::check_update_time(ctx, TARGET_FPS) {
+            if let Some(spaceship_id) = self.spaceship_id {
+                let spaceship = &mut self.objects[spaceship_id];
+                match self.controls.left_right {
+                    Some(Direction::Left) => spaceship.acc_x -= ACC_STEP_X,
+                    Some(Direction::Right) => spaceship.acc_x += ACC_STEP_X,
+                    _ => {
+                        spaceship.acc_x =
+                            spaceship.acc_x.signum() * (spaceship.acc_x.abs() - ACC_STEP_X)
+                    }
+                };
+
+                match self.controls.up_down {
+                    Some(Direction::Up) => spaceship.acc_y -= ACC_STEP_Y,
+                    Some(Direction::Down) => spaceship.acc_y += ACC_STEP_Y,
+                    _ => {
+                        spaceship.acc_y =
+                            spaceship.acc_y.signum() * (spaceship.acc_y.abs() - ACC_STEP_Y)
+                    }
+                };
+
+                spaceship.acc_x = na::clamp(spaceship.acc_x, -MAX_ACC_X, MAX_ACC_X);
+                spaceship.acc_y = na::clamp(spaceship.acc_y, -MAX_ACC_Y, MAX_ACC_Y);
+            }
+
+            for mut object in &mut self.objects {
+                object.vel_x += object.acc_x;
+                object.vel_y += object.acc_y;
+
+                object.pos_x += object.vel_x;
+                object.pos_y += object.vel_y;
+
+                if object.pos_x - object.size_x > 1.0 {
+                    object.pos_x = 0.0;
+                } else if object.pos_x + object.size_x < 0.0 {
+                    object.pos_x = 1.0;
+                }
+                if object.pos_y - object.size_y > 1.0 {
+                    object.pos_y = 0.0;
+                } else if object.pos_y + object.size_y < 0.0 {
+                    object.pos_y = 1.0;
+                }
+            }
+        }
+
         Ok(())
     }
 
@@ -90,13 +185,53 @@ impl EventHandler for SaveThePinkSkin {
                 ctx,
                 graphics::DrawMode::fill(),
                 na::Point2::new(0.0, 0.0),
-                object.size_x,
+                object.size_x * SCREEN_SIZE_X,
                 0.1,
                 object.color,
             )?;
-            graphics::draw(ctx, &circle, (na::Point2::new(object.pos_x, object.pos_y),))?;
+            graphics::draw(
+                ctx,
+                &circle,
+                (na::Point2::new(
+                    object.pos_x * SCREEN_SIZE_X,
+                    object.pos_y * SCREEN_SIZE_Y,
+                ),),
+            )?;
         }
 
         graphics::present(ctx)
+    }
+
+    /// key_down_event gets fired when a key gets pressed.
+    fn key_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        keycode: KeyCode,
+        _keymod: KeyMods,
+        _repeat: bool,
+    ) {
+        if let Some(dir) = from_keycode(keycode) {
+            match dir {
+                Direction::Up | Direction::Down => self.controls.up_down = Some(dir),
+                Direction::Left | Direction::Right => self.controls.left_right = Some(dir),
+            }
+        }
+    }
+
+    fn key_up_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymods: KeyMods) {
+        if let Some(dir) = from_keycode(keycode) {
+            match dir {
+                Direction::Up | Direction::Down => {
+                    if self.controls.up_down == Some(dir) {
+                        self.controls.up_down = None;
+                    }
+                }
+                Direction::Left | Direction::Right => {
+                    if self.controls.left_right == Some(dir) {
+                        self.controls.left_right = None
+                    }
+                }
+            }
+        }
     }
 }
